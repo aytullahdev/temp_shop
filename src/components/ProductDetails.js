ebcb8a939fa14d37ad405e9b348aa7e4d6d4aef0
aytullahdev <ayt.ullah.dev@gmail.com>
2023-04-06 16:29:15 +0600

    Problem resolved

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import img from "../img/1.png";
 import img2 from "../img/2.png";
